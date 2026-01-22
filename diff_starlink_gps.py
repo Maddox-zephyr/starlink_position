002763528331c5e6a2ab522dfc70cd52336a6d50
Paul Bouchier <paul.bouchier@gmail.com>
2026-01-22 20:07:07 +0530

    improved starlink position display

--- diff_starlink_gps.py
+++ diff_starlink_gps.py
@@ -16,6 +16,11 @@ def haversine(lat1, lon1, lat2, lon2):
     c = 2*asin(sqrt(a))
     return R * c
 
+def dd_to_dm(deg):
+    d = int(deg)
+    m = (deg - d) * 60
+    return d, m
+
 async def subscribe_with_source():
     # URI for your specific server address
     uri = "ws://192.168.1.116:80/signalk/v1/stream?subscribe=none"
@@ -30,10 +35,12 @@ async def subscribe_with_source():
         }
         await websocket.send(json.dumps(msg))
 
-        gps_lat = 0.0
-        gps_lon = 0.0
-        starlink_lat = 0.0
-        starlink_lon = 0.0
+        gps_lat = None
+        gps_lon = None
+        starlink_lat = None
+        starlink_lon = None
+        lat_hemisphere = 'N'
+        lon_hemisphere = 'E'
 
         while True:
             try:
@@ -50,14 +57,32 @@ async def subscribe_with_source():
                         elif (dict_digger.dig(update, "$source") == "signalk-starlink"):
                             starlink_lat = dict_digger.dig(update, "values", 0, "value", "latitude")
                             starlink_lon = dict_digger.dig(update, "values", 0, "value", "longitude")
+
                             if (starlink_lat is not None and starlink_lon is not None
                                 and gps_lat is not None and gps_lon is not None):
-                                print ("Starlink Lat: ", starlink_lat, "Lon: ", starlink_lon)
-                                print ("GPS Lat: ", gps_lat, "Lon: ", gps_lon)
-                                distance = haversine(gps_lat, gps_lon, starlink_lat, starlink_lon)
-                                print("Distance between GPS and Starlink: ", distance, "miles\n")
+
+                                if starlink_lat < 0.0:
+                                    lat_hemisphere = 'S'
+                                else:
+                                    lat_hemisphere = 'N'
+                                if starlink_lon < 0.0:
+                                    lon_hemisphere = 'W'
+                                else:
+                                    lon_hemisphere = 'E'
+
+                                slink_gps_delta = haversine(gps_lat, gps_lon, starlink_lat, starlink_lon)
+                                slink_lat_deg, slink_lat_min = dd_to_dm(starlink_lat)
+                                slink_lon_deg, slink_lon_min = dd_to_dm(starlink_lon)
+
+                                print(f'Starlink Position: {slink_lat_deg}° {slink_lat_min:.3f}\' , {slink_lon_deg}° {slink_lon_min:.3f}\'  GPS delta: {slink_gps_delta:.2f} miles')
+
+                                #print ("Starlink Lat: ", starlink_lat, "Lon: ", starlink_lon)
+                                #print ("GPS Lat: ", gps_lat, "Lon: ", gps_lon)
+                                #print("Distance between GPS and Starlink: ", distance, "miles\n")
+                            else:
+                                print("Incomplete position data:")
                         else:
-                            print("Failed to parse source info")
+                            print("Failed to parse source info: ",)
                             print (update)
 
             except Exception as e:
